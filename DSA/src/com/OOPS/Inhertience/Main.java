7df36e0d0892bb704216c8142daa6793ae6de801
Mahesh Kasbe <maheshkasbe010@gmail.com>
2022-01-22 21:10:51 +0530

    ..

--- DSA/src/com/OOPS/Inhertience/Main.java
+++ DSA/src/com/OOPS/Inhertience/Main.java
@@ -1,5 +1,7 @@
 package com.OOPS.Inhertience;
 
+import com.OOPS.Access.A;
+
 public class Main {
     public static void main(String[] args){
     // Box box1 = new Box(11,2,3);
@@ -11,5 +13,10 @@ public class Main {
     Boxheight box4 = new Boxheight(12, 23, 43, 45, 21);
     
     System.out.println(box4.l + " " + box4.h + " " + box4.w + " " + box4.weight + " " + box4.height);
+
+    // for access modifiers
+        A obj = new A();
+        
+
     }
 }
